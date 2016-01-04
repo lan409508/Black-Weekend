df54d19f46ec5263c16290d5acad2bac230112b2
+ <+>
2016-01-04 11:23:45 +0800

    first

---
+++ Black-Weekend/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Black-Weekend
+//
+//  Created by scjy on 16/1/4.
+//  Copyright © 2016年 练晓俊. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
