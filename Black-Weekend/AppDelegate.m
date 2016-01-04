5a01ba94f1b9b761d2961e7984656a6c62a9cb99
+ <+>
2016-01-04 15:05:36 +0800

    lian

--- Black-Weekend/AppDelegate.m
+++ Black-Weekend/AppDelegate.m
@@ -1,4 +1,4 @@
-//
+
 //  AppDelegate.m
 //  Black-Weekend
 //
@@ -18,6 +18,9 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     // Override point for customization after application launch.
+    
+    
+    
     self.window.backgroundColor = [UIColor whiteColor];
     [self.window makeKeyAndVisible];
     return YES;
