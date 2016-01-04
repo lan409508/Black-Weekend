c137ccf394b5dea8229d9c89de1ac1bb312ed91a
+ <+>
2016-01-04 18:32:34 +0800

    Cell

---
+++ Black-Weekend/Classes/main/Views/MainTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  MainTableViewCell.h
+//  Black-Weekend
+//
+//  Created by scjy on 16/1/4.
+//  Copyright © 2016年 练晓俊. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainTableViewCell : UITableViewCell
+
+@end
---
+++ Black-Weekend/Classes/main/Views/MainTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  MainTableViewCell.m
+//  Black-Weekend
+//
+//  Created by scjy on 16/1/4.
+//  Copyright © 2016年 练晓俊. All rights reserved.
+//
+
+#import "MainTableViewCell.h"
+
+@implementation MainTableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
