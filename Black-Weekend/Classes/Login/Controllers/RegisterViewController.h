03634dc31b9a4b9e1c6b8ff1e4689b855ccfd9d5
+ <+>
2016-03-02 21:53:54 +0800

    finish

---
+++ Black-Weekend/Classes/Login/Controllers/RegisterViewController.h
@@ -0,0 +1,13 @@
+//
+//  RegisterViewController.h
+//  Black-Weekend
+//
+//  Created by scjy on 16/3/2.
+//  Copyright © 2016年 练晓俊. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RegisterViewController : UIViewController
+
+@end
---
+++ Black-Weekend/Classes/main/Views/FooterCollectionView.m
@@ -0,0 +1,13 @@
+//
+//  FooterCollectionView.m
+//  Black-Weekend
+//
+//  Created by scjy on 16/3/1.
+//  Copyright © 2016年 练晓俊. All rights reserved.
+//
+
+#import "FooterCollectionView.h"
+
+@implementation FooterCollectionView
+
+@end
